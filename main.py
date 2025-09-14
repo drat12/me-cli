bf5f8f60d56866ab06e2c8e72434083cd8ca7431
drat12 <jhankdrat12@gmail.com>
2025-09-14 18:49:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def show_main_menu(number, balance, balance_expired_at):
 
     # Tabel: Menu Utama
     table = Table(title="Menu Utama", show_header=True, header_style="bold yellow", expand=True)
-    table.add_column("No", style="bold cyan", width=6, justify="left")
-    table.add_column("Menu", style="bold", justify="right")
+    table.add_column("No", style="bold cyan", width=6, justify="right")
+    table.add_column("Menu", style="bold", justify="left")
 
     table.add_row("1", "Login/Ganti akun")
     table.add_row("2", "Lihat Paket Saya")
